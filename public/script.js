539ede90d137708b952e10daa5a211334cae8803
Pkt-Rkt <danielchiscock@gmail.com>
2024-01-06 12:57:37 +1300

    Cap-1.5

--- public/script.js
+++ public/script.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event fired");
     const chatBox = document.getElementById("chat-box");
     const userInput = document.getElementById("user-input");
+    const sendButton = document.getElementById("send-button");
 
     function appendMessage(message, isUser) {
         const messageDiv = document.createElement("div");
         messageDiv.className = isUser ? "user-message" : "bot-message";
         messageDiv.textContent = message;
-        chatBox.insertBefore(messageDiv, chatBox.firstChild); // Insert the new message at the beginning
-        chatBox.scrollTop = 0; // Scroll to the top to keep the newest message at the bottom
+        chatBox.insertBefore(messageDiv, chatBox.firstChild);
+        chatBox.scrollTop = 0;
     }
 
     async function sendMessage() {
-        console.log("sendMessage function is triggered");
         const userMessage = userInput.value.trim();
         if (userMessage !== "") {
             appendMessage(`You: ${userMessage}`, true);
 
             try {
-                // Make an API request to the server for the AI response
                 const response = await fetch("/api/sendMessage", {
                     method: "POST",
                     headers: {
@@ -31,16 +31,15 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
 
                 const data = await response.json();
-                const botResponse = `AI Bot: ${data.response}`;
+                const botResponse = `Luna: ${data.response}`;
                 appendMessage(botResponse, false);
             } catch (error) {
                 console.error("Error:", error.message);
             }
 
-            // Clear the user input
             userInput.value = "";
         }
     }
 
-    window.sendMessage = sendMessage;
+    sendButton.addEventListener("click", sendMessage);
 });
